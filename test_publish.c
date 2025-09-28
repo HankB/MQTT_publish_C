bf1a76121aed19934074f7d022fb9d7b1d09ca7e
Hank Barta <hbarta@gmail.com>
2025-09-28 16:54:22 -0500

    options broker name

--- test_publish.c
+++ test_publish.c
@@ -6,12 +6,18 @@
 #include "publish_mqtt_msg.h"
 #include <unistd.h>
 
-int main()
+const char *broker = "localhost";
+
+int main(int argc, char **argv)
 {
+    if (argc > 1)
+    {
+        broker = argv[1];
+    }
     int rc;
     for (int i = 0; i < 10; i++)
     {
-        rc = publish_mqtt_msg("test/topic", "hello world", "localhost", 0);
+        rc = publish_mqtt_msg("test/topic", "hello world", broker, 0);
         printf("publish_mqtt_msg returned %d\n", rc);
         sleep(1);
     }
