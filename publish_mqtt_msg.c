68754ec7896749c0f8948fef5373663de0b2f471
Hank Barta <hbarta@gmail.com>
2025-09-28 15:09:41 -0500

    working publish example

--- publish_mqtt_msg.c
+++ publish_mqtt_msg.c
@@ -9,9 +9,11 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
+#include <string.h>
 
 static bool chatty = true; // control console output
 static MQTTClient client;
+static MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
 
 #define ID_LEN 30
 static char client_id[ID_LEN];
@@ -42,7 +44,7 @@ int publish_mqtt_msg(const char *topic, const char *payload, const char *broker,
     // copied, modified from https://www.hivemq.com/blog/implementing-mqtt-in-c/
     // which was not particularly helpful. Reverted to (mostly)
     // https://github.com/HankB/MQTT_will/blob/main/C/MQTT_will.c
-    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    // and then https://eclipse.dev/paho/files/mqttdoc/MQTTClient/html/pubsync.html
     conn_opts.keepAliveInterval = 10;
     conn_opts.cleansession = 1;
     // conn_opts.username = "username";
@@ -53,5 +55,25 @@ int publish_mqtt_msg(const char *topic, const char *payload, const char *broker,
             printf("MQTTClient_connect() returned %d\n", rc);
         return rc;
     }
+
+    MQTTClient_message pubmsg = MQTTClient_message_initializer;
+    MQTTClient_deliveryToken token;
+
+    #define TOPIC "This/is/only/a/test."
+    #define PAYLOAD "Had this been an actual emergency..."
+    #define TIMEOUT     10000L
+    pubmsg.payload = PAYLOAD;
+    pubmsg.payloadlen = strlen(PAYLOAD);
+    pubmsg.qos = QOS;
+    pubmsg.retained = 0;
+    MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
+    printf("Waiting for up to %d seconds for publication of %s\n"
+           "on topic %s for client with ClientID: %s\n",
+           (int)(TIMEOUT / 1000), PAYLOAD, TOPIC, client_id);
+    rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
+    printf("Message with delivery token %d delivered\n", token);
+    MQTTClient_disconnect(client, 10000);
+    MQTTClient_destroy(&client);
+
     return rc;
 }
