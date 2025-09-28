b29978948ee3ee6e3bc364718ff541909cf4be8d
Hank Barta <hbarta@gmail.com>
2025-09-27 23:32:57 -0500

    chattiness

--- publish_mqtt_msg.c
+++ publish_mqtt_msg.c
@@ -10,14 +10,14 @@
 #include <stdbool.h>
 #include <time.h>
 
-static bool chatty = false; // control console output
+static bool chatty = true; // control console output
 static MQTTClient client;
 
 #define ID_LEN 30
 static char client_id[ID_LEN];
 static const char client_prefix[] = "MQTT_test.";
 static time_t t;
-#define HOSTNAME_LEN    1024
+#define HOSTNAME_LEN 1024
 static char hostname[HOSTNAME_LEN];
 
 static int init_mqtt()
@@ -27,15 +27,15 @@ static int init_mqtt()
 int publish_mqtt_msg(const char *topic, const char *payload, const char *broker, int QOS)
 {
     int rc;
-        // prepare some strings used to communicate with broker
-    srand((unsigned) time(&t)); // seed RNG
+    // prepare some strings used to communicate with broker
+    srand((unsigned)time(&t)); // seed RNG
     snprintf(client_id, ID_LEN, "%s%8.8X", client_prefix, rand());
 
     if ((rc = MQTTClient_create(&client, broker, client_id,
                                 MQTTCLIENT_PERSISTENCE_NONE, NULL)) != MQTTCLIENT_SUCCESS)
     {
         if (chatty)
-            printf("Failed to create client, return code %d\n", rc);
+            printf("MQTTClient_create() returned %d\n", rc);
         return rc;
     }
 
@@ -47,6 +47,11 @@ int publish_mqtt_msg(const char *topic, const char *payload, const char *broker,
     conn_opts.cleansession = 1;
     // conn_opts.username = "username";
     // conn_opts.password = "password";
-    rc = MQTTClient_connect(client, &conn_opts);
+    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
+    {
+        if (chatty)
+            printf("MQTTClient_connect() returned %d\n", rc);
+        return rc;
+    }
     return rc;
 }
